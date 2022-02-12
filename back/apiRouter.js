7c3a84a8a26fc700b700976518bcbb4c6d755ab4
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-02-13 00:03:16 +0100

    installation express + creation apiRouter

---
+++ back/apiRouter.js
@@ -0,0 +1,36 @@
+// Routes
+
+/* 
+
+Home :
+
+    Signup:
+        POST => /signup
+
+    Login:
+        POST => /login
+
+User :
+
+    User info :
+        GET ⇒ /me
+        POST ⇒ /me
+        DELETE ⇒ /me
+
+    User posts :
+        GET ⇒ /me/posts
+
+Feed :
+
+    Posts :
+        GET ⇒ /post/all
+        POST ⇒ /post/publish
+        DELETE ⇒ /post/:id
+
+    Comments :
+        GET ⇒ /post/comments/:id
+
+    Likes :
+        POST ⇒ /like/:id
+
+*/
--- back/server.js
+++ back/server.js
@@ -1,10 +1,36 @@
-const http = require('http');
-require('dotenv').config()
+// Imports
 
-const server = http.createServer((req, res) => {
-    res.end('Voila la réponse du serveur BG !');
+require('dotenv').config()
+const express = require('express');
+
+
+// Instantiate server
+
+const server = express()
+
+
+// Configure routes 
+
+server.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send('<h1>Hello World </h1>')
 });
 
-console.log(process.env.TEST) // remove this after you've confirmed it working
+/* server.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // allow preflight
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+}); */
 
-server.listen(process.env.PORT || 3000);
+
+// launch server
+
+server.listen(process.env.PORTBACK, function(){
+    console.log('Serveur en écoute!');
+});
