ea341454f5892676a2a953d56741cea54da4b82c
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-03-09 19:54:45 +0100

    maj netlify

--- front/src/axios/axios.js
+++ front/src/axios/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 //axios.defaults.baseURL = 'http://localhost:3000/api/' ;
 
-axios.defaults.baseURL = 'https://my-project.herokuapp.com/' ;
+axios.defaults.baseURL = 'https://groupomania-tarikaboulhouda.herokuapp.com/' ;
 
 console.log(localStorage.getItem('token'));
 
