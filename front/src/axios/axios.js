da985cd1315d4ed68d8dbb0fd605fd896fc4b37a
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-03-11 10:10:56 +0100

    supression adresse heroku

--- front/src/axios/axios.js
+++ front/src/axios/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
 
-//axios.defaults.baseURL = 'http://localhost:3000/api/' ;
-
-axios.defaults.baseURL = 'https://groupomania-tarikaboulhouda.herokuapp.com/' ;
+axios.defaults.baseURL = 'http://localhost:3000/api/' ;
 
 console.log(localStorage.getItem('token'));
 
