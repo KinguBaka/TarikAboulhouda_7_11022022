9816239f6fb72cdd9cec7bc9e18316ac03daca6c
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-03-11 14:16:18 +0100

    test modification baseUrl

--- front/src/axios/axios.js
+++ front/src/axios/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 //axios.defaults.baseURL = 'http://localhost:3000/api/' ;
 
-axios.defaults.baseURL = 'https://groupomania-tarikaboulhouda.herokuapp.com/' ;
+axios.defaults.baseURL = 'https://groupomania-tarikaboulhouda.herokuapp.com/api/' ;
 
 console.log(localStorage.getItem('token'));
 
