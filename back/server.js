2bbdfc5b00d71169230ef40d7f2e9af6a2668ca6
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-05-04 13:01:28 +0200

    suppresion clg et autre

--- back/server.js
+++ back/server.js
@@ -23,7 +23,7 @@ server.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     // allow preflight
     if (req.method === 'OPTIONS') {
-        res.send(200);
+        res.sendStatus(200);
     } else {
         next();
     }
--- front/src/axios/axios.js
+++ front/src/axios/axios.js
@@ -3,6 +3,4 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3000/api/' ;
 
-console.log(localStorage.getItem('token'));
-
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
